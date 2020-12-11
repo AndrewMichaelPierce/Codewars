15b2898c29ee755ae3290dba4ec78093e213b91c
Andrew Pierce <andrew@andrewmpierce.com>
2020-12-10 22:33:43 -0800

    python challenge solved

---
+++ Python/7kyu/0004redKnight/rk.py
@@ -0,0 +1,24 @@
+def main():
+    def red_knight(N, P):
+        isOdd = False
+        if N % 2 != 0:
+            isOdd = True
+        while N != P:
+            P += 1
+        if isOdd:
+            if (P + 1) % 4 == 0:
+                return "Black", N + 1
+            else:
+                return "White", N + 1
+        else:
+            if P % 4 == 0:
+                return "White", N
+            else:
+                return "Black", N
+    red_knight(0, 4)
+
+
+if __name__ == "__main__":
+    main()
+
+# Status = Solved
