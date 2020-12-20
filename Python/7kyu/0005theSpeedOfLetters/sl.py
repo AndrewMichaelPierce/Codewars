307ea86a3ecf3389516d1e0df8cca3444da53166
Andrew Pierce <andrew@andrewmpierce.com>
2020-12-20 17:11:02 -0500

    fastestLetter

---
+++ Python/7kyu/0005theSpeedOfLetters/sl.py
@@ -0,0 +1,14 @@
+def main():
+    def speedify(st):
+        for letter in st:
+
+        print(ord(st))
+        pass
+    speedify("A")
+
+
+if __name__ == "__main__":
+    main()
+
+# ord("A") = 65
+# chr(65) = 'A'
