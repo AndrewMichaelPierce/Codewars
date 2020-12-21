76cdcd6353e106d1a9ff7f13d67d61546ec8ad6b
Omtaga <andrew@andrewmpierce.com>
2020-12-21 02:43:34 -0500

    kata solved

--- Python/7kyu/0005theSpeedOfLetters/sl.py
+++ Python/7kyu/0005theSpeedOfLetters/sl.py
@@ -2,16 +2,21 @@ def main():
     def speedify(st):
         biggest = 0
         result = []
+        initialMove = 0
         for letter in st:
-            if ord(letter) > biggest:
-                biggest = ord(letter)
+            if ord(letter) - 64 + initialMove > biggest:
+                biggest = ord(letter) - 64 + initialMove
+            initialMove += 1
         for index in range(biggest):
             result.append(" ")
+        move = 0
         for letter in st:
-            letterValue = ord(letter) - 64
+            letterValue = ord(letter) - 65 + move
             result[letterValue] = letter
-        print(result.join())
-    speedify("A")
+            move += 1
+        print("".join(result))
+        return "".join(result)
+    speedify("HELLOWORLD")
 
 
 if __name__ == "__main__":
