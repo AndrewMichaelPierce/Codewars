62fec82e154ed0d5be8c0ffb1e04651e1d84799f
Omtaga <andrew@andrewmpierce.com>
2020-12-21 20:55:26 -0500

    is square in an multi-dimensional array

---
+++ JavaScript/6kyu/0021areTheySquare2/ats.js
@@ -0,0 +1,13 @@
+let isSquare = function(arr) {
+    function innerArray(arr) {
+        function prime(input) {
+
+        }
+        if(Array.isArray(arr)) {
+
+        }
+    }
+    for(let i = 0, len = arr.length(); i < len; i++) {
+        for (let )
+    }
+}
