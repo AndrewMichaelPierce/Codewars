55c44401a96d3b092abc26222aec2df94bf87ac9
Andrew Pierce <andrew@andrewmpierce.com>
2020-12-22 22:59:51 -0500

    kata solved

--- JavaScript/6kyu/0021areTheySquare2/ats.js
+++ JavaScript/6kyu/0021areTheySquare2/ats.js
@@ -1,29 +1,40 @@
 let isSquare = function(arr) {
-    function innerArray(arr) {
-        function prime(input) {
-
-        }
-        if(Array.isArray(arr)) {
-
-        }
-    }
-
-    function isSquare(input) {
-        if (input === 1) {
-            console.log("True");
-            return true;
-        }
-        let halfInput = input / 2;
-        for (let i = 1; i <= halfInput; i++) {
-            if (i * i === input) {
-                console.log("True");
-                return true;
+    function printNestedArray(arr) {
+        if (Array.isArray(arr)) {
+            for (let i = 0, len = arr.length; i < len; i++) {
+                if (Array.isArray(arr[i])) {
+                    if (printNestedArray(arr[i]) === false) {
+                      return false;
+                    }
+                } else {
+                    if (isSquare(arr[i]) === false) {
+                        return false;
+                    }
+                }
+            }
+        } else {
+            if (isSquare(arr) === false) {
+                return false;
             }
         }
-        console.log("False");
-        return false;
     }
-    for(let i = 0, len = arr.length(); i < len; i++) {
-        for (let )
+    function isSquare(input) {
+        if (input === 1 || input === 0) {
+        } else {
+            let halfInput = input / 2;
+            for (let i = 1; i <= halfInput; i++) {
+                if (i * i === input) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+    if (arr.length === 0) {
+        return undefined;
+    } else {
+        return printNestedArray(arr) !== false;
     }
 }
+
+// Status: Solved
--- JavaScript/6kyu/0021areTheySquare2/prtNestedArray.js
+++ JavaScript/6kyu/0021areTheySquare2/prtNestedArray.js
@@ -1,6 +1,16 @@
 function printNestedArray(arr) {
-
+    if (Array.isArray(arr)) {
+        for (let i = 0, len = arr.length; i < len; i++) {
+            if (Array.isArray(arr[i])) {
+                printNestedArray(arr[i]);
+            } else {
+                console.log(arr[i]);
+            }
+        }
+    } else {
+        console.log(arr);
+    }
 }
 
-let arr = []
+let arr = [1, [4], [9, 16, 25], [36, 49, [64, 81]], [100, [121, 144, [169]]], [196, [225, [256, 111, [324, [361, 400]]]]]];
 printNestedArray(arr);
